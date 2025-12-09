ca16be5b39e75c1c21d308ca9f425da72e3c5e93
seambad <111530108+seambad@users.noreply.github.com>
2025-12-09 11:54:25 +0300

    Update 123.lua

--- +w tech.lua
+++ +w tech.lua
@@ -1,6 +1,6 @@
 
 -- Переменная для хранения текущей вкладки
- pizdecSS = {
+pizdecSS = {
     ["•  [hvhserver.xyz] roll fix"] = "62.122.214.55:27015",
     ["•  HackHaven HvH"] = "46.174.55.54:27015",
     ["•  eXpidors.Ru"] = "46.174.51.137:7777",
@@ -10,17 +10,19 @@
     ["•  SharkProject | MM"] = "37.230.228.148:27015",
     ["•  WhiteProject"] = "46.174.49.161:1337",
     ["•  LivixProject HVH"] = "185.9.145.159:28423",
-
 }
+
 clipboard = require("gamesense/clipboard")
- servs = {}
+
+servs = {}
 for k, v in pairs(pizdecSS) do
     table.insert(servs, k)
 end
--- Элементы для вкладки Servers
-zalupenko = ui.new_listbox("lua", "b", "Connects", servs )
 
-Connects = ui.new_button("lua", "b", "\r Connect", function()
+-- Элементы создаются только в разделе Lua → B
+zalupenko = ui.new_listbox("lua", "b", "Серверы", "Выберите сервер", servs)
+
+Connects = ui.new_button("lua", "b", "Подключиться", function()
     local index = ui.get(zalupenko)
     local i = 0
 
@@ -32,7 +34,8 @@ Connects = ui.new_button("lua", "b", "\r Connect", function()
     end
     client.exec("connect " .. servers)
 end)
-Copyss = ui.new_button("lua", "b", "\r Copy ip-address", function()
+
+Copyss = ui.new_button("lua", "b", "Скопировать IP", function()
     local index = ui.get(zalupenko)
     local i = 0
 
@@ -42,14 +45,12 @@ Copyss = ui.new_button("lua", "b", "\r Copy ip-address", function()
         end
         i = i + 1
     end
-	clipboard.set(servers)
-end)
-RetrySS = ui.new_button("lua", "b", "\r Rejoin (Retry)", function()
-	client.exec("disconnect; retry")
+    clipboard.set(servers)
 end)
 
-
-
+RetrySS = ui.new_button("lua", "b", "Переподключиться (Retry)", function()
+    client.exec("disconnect; retry")
+end)
 
 local obex_data = obex_fetch and obex_fetch() or {username = 'User', build = 'BETA', discord=''}
 
@@ -15747,4 +15748,5 @@ end
 ui_set_callback(master_switch, interface_callback)
 interface_callback(master_switch) do
 end
+
 
